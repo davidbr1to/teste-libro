8c06c91eeb89dd4218f6d309242dba3f0442cbc3
Davidbr1to <david.nrpg@gmail.com>
2020-11-28 10:47:33 -0300

    Atualizando controller de Aluno e suas rotas

--- app/Http/Controllers/AlunoController.php
+++ app/Http/Controllers/AlunoController.php
@@ -3,8 +3,38 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Aluno;
 
 class AlunoController extends Controller
 {
-    //
+    public function index()
+    {
+        return Aluno::all();
+    }
+
+    public function show($email)
+    {
+        return Aluno::find($email);
+    }
+
+    public function store(Request $request)
+    {
+        return Aluno::create($request->all());
+    }
+
+    public function update(Request $request, $id)
+    {
+        $aluno = Aluno::find($id);
+        $aluno->update($request->all());
+
+        return $article;
+    }
+
+    public function delete(Request $request, $id)
+    {
+        $aluno = Aluno::find($id);
+        $aluno->delete();
+
+        return 204;
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AlunoController;
+use App\Models\Aluno;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +16,37 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('alunos', function(){
+    return Aluno::all();
+});
+
+Route::get('alunos/{email}', function($email){
+    return Aluno::find($email);
+});
+
+Route::post('alunos', function(Request $request){
+    return Aluno::create($request->all());
+});
+
+Route::put('alunos/{id}', function(Request $request, $id) {
+    $aluno = Aluno::findOrFail($id);
+    $aluno->update($request->all());
+
+    return $aluno;
+});
+
+Route::delete('alunos/{id}', function($id) {
+    Aluno::find($id)->delete();
+
+    return 204;
+});
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('alunos', 'App\Http\Controllers\AlunoController@index');
+Route::get('alunos/{id}', 'App\Http\Controllers\AlunoController@show');
+Route::post('alunos', 'App\Http\Controllers\AlunoController@store');
+Route::put('alunos/{id}', 'App\Http\Controllers\AlunoController@update');
+Route::delete('articles/{id}', 'App\Http\Controllers\AlunoController@delete');
